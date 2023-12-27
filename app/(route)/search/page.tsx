cd2f346bec134823741aed2a3ea5c9c04b1cc9eb
Moon Dahyun <dahyun723@ajou.ac.kr>
2023-12-28 02:42:49 +0900

    feat : 검색바'

--- app/(route)/search/page.tsx
+++ app/(route)/search/page.tsx
@@ -1,6 +1,39 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
+import styled from "styled-components";
 
 export default function Search() {
-  return <p>검색 페이지</p>;
+  return (
+    <SearchWrapper>
+      <SearchBar placeholder="어떤 책을 찾고 있나요?" />
+    </SearchWrapper>
+  );
 }
+
+const SearchWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-top: 172px;
+`;
+
+const SearchBar = styled.input.attrs((props) => ({
+  type: "text",
+  placeholder: props.placeholder,
+}))`
+  width: 700px;
+  height: 80px;
+  padding-left: 23px;
+
+  color: #fbff48;
+  font-size: 32px;
+
+  border: 5px solid #fbff48;
+  background-color: rgba(217, 217, 217, 0);
+
+  &&::placeholder {
+    padding-left: 23px;
+    color: #fbff48;
+    font-size: 32px;
+  }
+`;
