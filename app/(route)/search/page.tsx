8c625a5cdeba32b5def62c5e090ff706e620e734
Moon Dahyun <dahyun723@ajou.ac.kr>
2023-12-28 02:54:05 +0900

    feat: 더미데이터(책) 검색결과 추가

--- app/(route)/search/page.tsx
+++ app/(route)/search/page.tsx
@@ -2,17 +2,31 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import styled from "styled-components";
+import BookImage1 from "../../_assets/images/dummy_book_1.png";
+import BookImage2 from "../../_assets/images/dummy_book_2.png";
+import BookImage3 from "../../_assets/images/dummy_book_3.png";
+import BookImage4 from "../../_assets/images/dummy_book_4.png";
+import BookImage5 from "../../_assets/images/dummy_book_5.png";
 
 export default function Search() {
   return (
     <SearchWrapper>
       <SearchBar placeholder="어떤 책을 찾고 있나요?" />
+      <ResultWrapper>
+        <Image src={BookImage1} alt="책이미지1" />
+        <Image src={BookImage2} alt="책이미지2" />
+        <Image src={BookImage3} alt="책이미지3" />
+        <Image src={BookImage4} alt="책이미지4" />
+        <Image src={BookImage5} alt="책이미지5" />
+      </ResultWrapper>
     </SearchWrapper>
   );
 }
 
 const SearchWrapper = styled.div`
   display: flex;
+  flex-direction: column;
+  align-items: center;
   justify-content: center;
   padding-top: 172px;
 `;
@@ -24,6 +38,7 @@ const SearchBar = styled.input.attrs((props) => ({
   width: 700px;
   height: 80px;
   padding-left: 23px;
+  margin-bottom: 141px;
 
   color: #fbff48;
   font-size: 32px;
@@ -36,4 +51,10 @@ const SearchBar = styled.input.attrs((props) => ({
     color: #fbff48;
     font-size: 32px;
   }
+`;
+
+const ResultWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
