498cb220a3322b3585be2680f7996c1ffe393af1
Moon Dahyun <dahyun723@ajou.ac.kr>
2023-12-29 14:52:54 +0900

    fix: 리액트쿼리 초기세팅

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,24 @@
-import type { Metadata } from "next";
+"use client";
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import "./globals.css";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const queryClient = new QueryClient();
+
   return (
-    <html lang="en">
-      <body>
-        <div className="wrapper">{children}</div>
-      </body>
-    </html>
+    <QueryClientProvider client={queryClient}>
+      <html lang="en">
+        <body>
+          <div className="wrapper">{children}</div>
+        </body>
+        <ReactQueryDevtools />
+      </html>
+    </QueryClientProvider>
   );
 }
